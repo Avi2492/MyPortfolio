fe2fa5dd26e71fcf772dfda51afa50ad3996c294
AVINASH PANDEY <98386256+Avi2492@users.noreply.github.com>
2023-03-22 19:16:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// navbar
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+// projects
+var swiper = new Swiper(".project-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
