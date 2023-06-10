363b71bff8c2e15b91f54f2d92ee78f2a76d3573
AVINASH PANDEY <98386256+Avi2492@users.noreply.github.com>
2023-06-11 00:14:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,3 +30,40 @@ var swiper = new Swiper(".project-slider", {
     },
   },
 });
+
+// Running animation
+const text = "Avinash Pandey";
+
+const typedText = document.getElementById("typed-text");
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+  const currentText = typedText.textContent;
+
+  if (!isDeleting && charIndex <= text.length) {
+    typedText.textContent = text.substring(0, charIndex);
+    charIndex++;
+  } else if (isDeleting && charIndex >= 0) {
+    typedText.textContent = text.substring(0, charIndex);
+    charIndex--;
+  }
+
+  if (charIndex === text.length + 1) {
+    // Start deleting after a short pause
+    isDeleting = true;
+    setTimeout(type, 500);
+  } else if (charIndex === -1) {
+    // Switch to typing mode again
+    isDeleting = false;
+    charIndex = 0;
+    setTimeout(type, 500);
+  } else {
+    // Continue typing or deleting
+    const typingSpeed = isDeleting ? 100 : 200;
+    setTimeout(type, typingSpeed);
+  }
+}
+
+type();
+
